90b02b6b0db0b181a5a95dc83a51fc05d9dcdffa
Craig Horsborough <craighorsborough@gmail.com>
2014-12-08 18:32:50 +0000

    Hmm

--- lib/server.rb
+++ lib/server.rb
@@ -6,7 +6,7 @@ require 'rack-flash'
 
 env = ENV["RACK_ENV"] || "development"
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-DataMapper.setup(:default, DATABASE_URL)
+DataMapper.setup(:default, "postgres://lurukbtubkrzhe:UQSGG7BN7Bf1TRNk2yT2Iiy03R@ec2-107-20-245-187.compute-1.amazonaws.com:5432/db41gpgnabtolc")
 
 require './lib/link' # this needs to be done after datamapper is initialised
 require './lib/tag'
