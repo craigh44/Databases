ee43ff716781eeb7ccf10317e00da4aae48a1a82
Craig Horsborough <craighorsborough@gmail.com>
2014-12-11 10:11:51 +0000

    Styled sign out button

--- lib/server.rb
+++ lib/server.rb
@@ -3,7 +3,6 @@ require 'sinatra'
 require 'rack-flash'
 require './lib/helpers/helpers.rb'
 
-
 require './lib/link' # this needs to be done after datamapper is initialised
 require './lib/tag'
 require './lib/user'
@@ -12,15 +11,12 @@ require './lib/controllers/tags.rb'
 require './lib/controllers/links.rb'
 
 require_relative 'data_mapper_setup'
-
 enable :sessions
 set :session_secret, 'super secret'
 use Rack::Flash 
 set :views, Proc.new { File.join(root, "..", "views") }
 
-
-
- get '/' do
+get '/' do
   @links = Link.all
   erb :index
 end
