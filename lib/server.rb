9fbf2d432072beb014a7f0e490410ef0f93c265d
Craig Horsborough <craighorsborough@gmail.com>
2014-12-11 09:12:33 +0000

    Took out tags for now, keeping it simple

--- lib/server.rb
+++ lib/server.rb
@@ -32,9 +32,9 @@ post '/links' do
   user_id = current_user.id
   url = params["url"]
   title = params["title"]
-  tags = params["tags"].split(" ").map{|tag| 
-  	Tag.first_or_create(:text => tag)}
-  Link.create(:url => url, :title => title, :tags => tags, :user_id => user_id  )
+  # tags = params["tags"].split(" ").map{|tag| 
+  # 	Tag.first_or_create(:text => tag)}
+  Link.create(:url => url, :title => title, :user_id => user_id  )
   redirect to('/')
 end
 
