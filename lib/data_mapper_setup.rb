156869ee4172f0a58c560e9e47cf242dd0ab9856
Craig Horsborough <craighorsborough@gmail.com>
2014-12-11 12:13:01 +0000

    Reset db

--- lib/data_mapper_setup.rb
+++ lib/data_mapper_setup.rb
@@ -1,6 +1,6 @@
 env = ENV["RACK_ENV"] || "development"
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-DataMapper.setup(:default, ENV["DATABASE_URL"])
+DataMapper.setup(:default, "postgres://lurukbtubkrzhe:UQSGG7BN7Bf1TRNk2yT2Iiy03R@ec2-107-20-245-187.compute-1.amazonaws.com:5432/db41gpgnabtolc")
 
 
 # After declaring your models, you should finalise them
